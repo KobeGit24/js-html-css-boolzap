89bb629eb493f124c196dec0328bf3038e6dc6c4
Stefano <stefano.zaccaria29@gmail.com>
2020-07-13 21:22:01 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 function init() {
-    message();
+    $('#invio').click(message);
     $('#user-msg').keyup(press);
 }
 
 
 // function
 function message() {
-    $('#invio').click(function(){
         userInput = $('#user-msg').val();
         var tem = $('.template #my-answer').clone();
         tem.addClass('right');
@@ -18,22 +17,11 @@ function message() {
             $('#chat').append(risp);
         },1000)
         $('#user-msg').val("");
-    });
 }
 
     function press (){
     if (event.which==13) {
-        userInput = $('#user-msg').val();
-        var tem = $('.template #my-answer').clone();
-        tem.addClass('right');
-        tem.children('.msg').append(userInput);
-        $('#chat').append(tem);
-        setTimeout(function(){
-            var risp = $('.template #com-answer').clone();
-            risp.addClass('left');
-            $('#chat').append(risp);
-        },1000)
-        $('#user-msg').val("");  
+        message();
     }
 };
 
