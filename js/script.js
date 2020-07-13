424668df47b95253b3a8758551aa75cbf35a180b
Stefano <stefano.zaccaria29@gmail.com>
2020-07-13 17:54:20 +0200

    !

--- js/script.js
+++ js/script.js
@@ -6,9 +6,16 @@
 function init() {
     $('#invio').click(function(){
         userInput = $('#user-msg').val();
-        $('#user-msg').val().addClass('user-mess');
-        $('#chat').append(userInput).
-        console.log($('#user-msg').val());
+        var tem = $('.template #my-answer').clone();
+        tem.addClass('right');
+        tem.children('.msg').append(userInput);
+        $('#chat').append(tem);
+        setTimeout(function(){
+            var risp = $('.template #com-answer').clone();
+            risp.addClass('left');
+            $('#chat').append(risp);
+        },1000)
+        console.log(userInput);
         $('#user-msg').val("");
     });
 
